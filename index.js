7c71ffa4a8e0345252c3b19b20839cca4e949b7f
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-11-27 22:23:59 +0530

    feat: new feature added
    - select model
    - add token key or use key from .env option
    - some styling

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import { InferenceClient } from "@huggingface/inference";
+import { marked } from "marked"; //https://marked.js.org/
 
-// Add loading screen
-// Add model selection
-// Maybe add cache system
-/// if the same msg is asked rather that fetching from ai serve form the cache (also tell the user it is being serverd from cache and weather if they want it from the ai)
-// render the response for any md or meramid 
-/// present it beautifully
+/*
+  TODO: 
+  All messages and responses will be stored in local or session storage.
+  If the same message is requested then it will be served from that storage rather than fetching from AI.
+  A small info message will be also beshown notifing the user that the response they are seeing is being served from cache.
+  Also with an option to get fresh response rather than cached response.
+*/
 
-let modelName = "mistralai/Mistral-7B-Instruct-v0.2:featherless-ai";
-// let providerName = "featherless-ai"
-let systemContent = "You are a helpful assistant.";
-let userContent = "How may color are there in a rainbow?";
+let modelName; // example: mistralai/Mistral-7B-Instruct-v0.2:featherless-ai
+let providerName; // example: "featherless-ai"
+let systemContent = "You are a helpful assistant!";
+let userContent;
+let maxToken = 256; // 256
+let aiTemperature; // 0.9
 
 const enterMessage = document.querySelector("#enter-message");
 const sendMessageBtn = document.querySelector("#send-msg");
@@ -19,35 +23,49 @@ const displayResponse = document.querySelector("#response");
 sendMessageBtn.addEventListener("click", getResponse);
 
 async function getResponse() {
-  // Fail on empty input
-  if (enterMessage.value === "") {
-    alert("Input is blank");
-    return console.log("Input is blank");
+  modelName = document.querySelector("#select-model").value;
+  if (!modelName) {
+    alert("Select a model");
+    return console.log("Select a model");
   }
 
-  // Get & apply input
+  if (enterMessage.value === "") {
+    alert("Message input is empty!");
+    return console.log("Message input is empty!");
+  }
+
   userContent = enterMessage.value;
 
-  const client = new InferenceClient(import.meta.env.VITE_HF_TOKEN);
-  const chatCompletion = await client.chatCompletion({
-    model: `${modelName}`,
-    // provider: `${providerName}`,
-    messages: [
-      { role: "system", content: `${systemContent}` },
-      {
-        role: "user",
-        content: `${userContent}`,
-      },
-    ],
-    max_tokens: 256,
-    temperature: 0.9,
-  });
+  const client = new InferenceClient(
+    document.querySelector("#api-key").value
+      ? document.querySelector("#api-key").value
+      : import.meta.env.VITE_HF_TOKEN
+  );
 
-  renderResponse(chatCompletion);
+  try {
+    document.querySelector(".loader").style = "display: block";
+    const chatCompletion = await client.chatCompletion({
+      model: `${modelName}`,
+      ...(providerName ? { provider: providerName } : {}),
+      messages: [
+        { role: "system", content: systemContent },
+        {
+          role: "user",
+          content: userContent,
+        },
+      ],
+      ...(maxToken ? { max_tokens: maxToken } : {}),
+      ...(aiTemperature ? { temperature: aiTemperature } : {}),
+    });
+    renderResponse(chatCompletion);
+  } catch (error) {
+    console.error(error);
+    document.querySelector(".loader").style = "display: none";
+  }
 }
 
 async function renderResponse(resp) {
-  displayResponse.textContent = `${resp.choices[0].message.content}`;
-  console.log(resp);
-  console.log(resp.choices[0].message);
+  document.querySelector(".loader").style = "display: none";
+  displayResponse.style = "display: block";
+  displayResponse.innerHTML = marked.parse(resp.choices[0].message.content);
 }
